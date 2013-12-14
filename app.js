611f5aafcdae09ca9eeb74fdded140ba2a87f9ab
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2013-12-14 14:28:56 +0200

    POST and GET stuff

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var redis = require('redis');
+var _ = require('underscore');
 var app = express();
-var port = process.env.PORT || 1337;
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
+var redisClient = redis.createClient();
+app.use(express.bodyParser());
+
+
+app.post('/api/:errorHash', function(req, res){
+  res.send('ok')
+  _.each(req.body, function (value, key) {
+    console.log(key+': '+value);
+    redisClient.hset(req.params.errorHash, key, value, redis.print);
+  });
 });
 
-app.listen(port);
-console.log('Listening on port '+port);
+app.get('/api/:errorHash', function(req, res){
+  redisClient.hgetall(req.params.errorHash, function (err, replies) {
+    console.log(replies);
+    res.end(JSON.stringify(replies));
+  });
+});
+
+app.listen(3000);
+console.log('Listening on port '+3000);
