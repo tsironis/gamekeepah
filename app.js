5de80e1355b8cd7fe49a2bac80e62c28eb08216e
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2013-12-14 12:55:40 +0200

    Fixes port for azure

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 1337;
 app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(port);
+console.log('Listening on port '+port);
